b89c1e89c48b8cd96212ffa140da969b738a7da5
hakanErgin <hakan.ergin@gmail.com>
2020-02-14 14:38:10 +0100

    added filter function

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,11 +1,49 @@
-import React from 'react'; 
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
-function Home() { 
+function Home() {
+  const [articles, setArticles] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(function() {
+    async function getArticles() {
+      try {
+        const response = await axios.get('/api/articles');
+        setArticles(response.data);
+      } catch (error) {
+        console.log('error', error);
+      }
+    }
+    getArticles();
+  }, []);
+
+  console.log(articles);
+
+  const handleChange = e => {
+    setSearchTerm(e.target.value);
+  };
+
+  useEffect(() => {
+    const results = articles.filter(article =>
+      article.title.includes(searchTerm)
+    );
+    setSearchResults(results);
+  }, [searchTerm]);
+
   return (
-    <div className="jumbotron">
-      <h1>Home Page</h1>
+    <div>
+      <input type="text" value={searchTerm} onChange={handleChange} />
+      <ul>
+        {searchResults.map(item => (
+          <li>
+            {item.title} - {item.content}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
 
-export default Home; 
+export default Home;
