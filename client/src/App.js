fb8c048569103d8f2f4fefc794fc566529501335
hakanErgin <hakan.ergin@gmail.com>
2020-02-14 16:03:14 +0100

    structured for this project

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, NavLink, Switch} from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  NavLink,
+  Switch
+} from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/Home';
-import ArticleList from './components/articles/ArticleList';
 import ArticleInfo from './components/articles/ArticleInfo';
 import ArticleAdd from './components/articles/ArticleAdd';
 import ArticleEdit from './components/articles/ArticleEdit';
 
 function App() {
   return (
-    <div className="App">     
+    <div className="App">
       <Router>
         <Navigation />
         <div className="container">
@@ -21,23 +25,21 @@ function App() {
 }
 
 function Navigation() {
-  return(
+  return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
-      <div className='container'>
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item"><NavLink exact className="nav-link" activeClassName="active" to="/">Home</NavLink></li>
-          <li className="nav-item"><NavLink exact className="nav-link" activeClassName="active" to="/articles">Articles</NavLink></li>
-        </ul>
+      <div className="container">
+        <NavLink exact className="nav-link" activeClassName="active" to="/">
+          Home
+        </NavLink>
       </div>
     </nav>
   );
 }
 
 function Main() {
-  return(
+  return (
     <Switch>
       <Route exact path="/" component={Home} />
-      <Route exact path="/articles" component={ArticleList} />
       <Route exact path="/articles/new" component={ArticleAdd} />
       <Route exact path="/articles/:_id" component={ArticleInfo} />
       <Route exact path="/articles/:_id/edit" component={ArticleEdit} />
